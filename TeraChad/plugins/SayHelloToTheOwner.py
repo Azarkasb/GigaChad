e4594e0983b937525ef35da4bf08d7c234d1695d
Mohammad Ali Azarkasb <azarkasbma@gmail.com>
2021-11-30 12:48:22 +0330

    Add Hi Command to the bot

---
+++ TeraChad/plugins/SayHelloToTheOwner.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters
+from plugins import admins, myConnector, myCommands
+
+
+# Say Hi and report important data from the DB
+@Client.on_message(filters.user(admins) & filters.command('hi'))
+def echo_hi(client, message):
+    client.send_message(message.chat.id, 'Hello')
+    sql = 'SELECT COUNT(*) FROM chats WHERE State = TRUE'
+    myCommands.execute(sql)
+    myResults = myCommands.fetchone()
+    msg = f'I am active on {myResults[0]} chat(s)!'
+    client.send_message(message.chat.id, msg)
